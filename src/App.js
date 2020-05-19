f664f2698372732a546f48f5d78bee0eeaf420d2
David Egerton <david.egertonsmith@gmail.com>
2020-05-19 21:05:25 +0100

    built city list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import CityList from './components/CityList';
+
+const cities = [
+  { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
+  { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
+  { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
+];
+
 
 function App() {
   return (
     <div className="App">
-      <CityList />
+      <CityList cities={cities} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
-import '../assets/stylesheets/application.scss';
+import './App.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
